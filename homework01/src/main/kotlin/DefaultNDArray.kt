dc151ae346c4f96f2be6f2fafc768dfe412152cb
Oratovskii_Oleg <oleg_oratovskiy@mail.ru>
2022-10-23 19:00:03 +0300

    kotlin-hw-01-olegoratovskiy

--- homework01/src/main/kotlin/DefaultNDArray.kt
+++ homework01/src/main/kotlin/DefaultNDArray.kt
@@ -1,5 +1,4 @@
-
-interface NDArray: SizeAware, DimentionAware {
+interface NDArray : SizeAware, DimensionAware {
     /*
      * Получаем значение по индексу point
      *
@@ -83,11 +82,193 @@ interface NDArray: SizeAware, DimentionAware {
  *
  * Инициализация - через factory-методы ones(shape: Shape), zeros(shape: Shape) и метод copy
  */
-class DefaultNDArray: NDArray {
+class DefaultNDArray private constructor(private val shape: Shape, private val values: IntArray) : NDArray {
+    override val ndim: Int
+        get() = shape.ndim
+
+    override val size: Int
+        get() = shape.size
+
+    override fun dim(i: Int): Int = shape.dim(i)
+
+    companion object {
+        fun zeros(shape: Shape): NDArray {
+            return DefaultNDArray(shape, IntArray(shape.size) { 0 })
+        }
+
+        fun ones(shape: Shape): NDArray {
+            return DefaultNDArray(shape, IntArray(shape.size) { 1 })
+        }
+    }
+
+    override fun at(point: Point): Int {
+        validatePoint(point)
+        return values[findIndex(point)]
+    }
+
+    override fun set(point: Point, value: Int) {
+        validatePoint(point)
+        values[findIndex(point)] = value
+    }
+
+    override fun copy(): NDArray {
+        return DefaultNDArray(shape, values.copyOf())
+    }
+
+    override fun view(): NDArray {
+        return DefaultNDArray(shape, values)
+    }
+
+    override fun add(other: NDArray) {
+        if (ndim == other.ndim) {
+            checkShapeEquality(other)
+            var point: Point? = DefaultPoint(*IntArray(ndim) { 0 })
+            while (point != null) {
+                set(point, at(point) + other.at(point))
+                point = getNextPoint(point)
+            }
+        } else if (ndim == other.ndim + 1) {
+            val extraDimInd = getPositionOfExtraDimension(other)
+            var point: Point? = DefaultPoint(*IntArray(ndim) { 0 })
+            while (point != null) {
+                set(point, at(point) + other.at(cutDimensionFromPoint(point, extraDimInd)))
+                point = getNextPoint(point)
+            }
+        } else {
+            throw NDArrayException.IllegalNDArrayDimensionException(
+                "expected `this` and `other` to have same dimensions"
+            )
+        }
+    }
+
+    override fun dot(other: NDArray): NDArray {
+        if (ndim != 2) {
+            throw NDArrayException.IllegalNDArrayDimensionException("expected 'this' to be two-dimensional");
+        }
+        return if (other.ndim == 2 && dim(1) == other.dim(0)) {
+            matricesMult(other)
+        } else if (other.ndim == 1 && this.dim(1) == other.dim(0)) {
+            matricesMult(convert1DArrayTo2DArray(this.dim(0), 1, other))
+        } else if (other.ndim == 1 && this.dim(1) == 1) {
+            matricesMult(convert1DArrayTo2DArray(this.dim(0), other.dim(0), other))
+        } else {
+            throw NDArrayException.IllegalNDArrayDimensionException("expected `other` to be another dimensional")
+        }
+    }
+
+    private fun findIndex(point: Point): Int {
+        var indexInValues = 0
+        var dimensionMult: Int = size
+        for (index in 0 until point.ndim) {
+            dimensionMult /= shape.dim(index)
+            indexInValues += point.dim(index) * dimensionMult
+        }
+        return indexInValues
+    }
+
+    private fun getNextPoint(point: Point): Point? {
+        val coords = IntArray(point.ndim)
+        for (index in 0 until point.ndim) {
+            coords[index] = point.dim(index)
+        }
+        var foundNext = false
+        for (index in point.ndim - 1 downTo 0) {
+            if (coords[index] < dim(index) - 1) {
+                ++coords[index]
+                foundNext = true
+                for (innerIndex in index + 1 until point.ndim) {
+                    coords[innerIndex] = 0
+                }
+                break
+            }
+        }
+        return if (foundNext) DefaultPoint(*coords) else null
+    }
+
+    private fun getPositionOfExtraDimension(other: NDArray): Int {
+        var extraDimension = -1
+        for (thisIndex in 0 until ndim) {
+            val otherIndex = thisIndex - (if (extraDimension != -1) 1 else 0)
+            if (otherIndex == other.ndim) {
+                extraDimension = thisIndex
+                break
+            }
+            if (other.dim(otherIndex) != dim(thisIndex)) {
+                if (extraDimension != -1) {
+                    throw NDArrayException.IllegalNDArrayDimensionException(
+                        "found more than one extra dimensions in this NDArray"
+                    )
+                }
+                extraDimension = thisIndex
+            }
+        }
+        if (extraDimension == -1) {
+            throw NDArrayException.IllegalNDArrayDimensionException(
+                "found no extra dimensions in this NDArray"
+            )
+        }
+        return extraDimension
+    }
+
+    private fun cutDimensionFromPoint(point: Point, position: Int): Point {
+        val coords = IntArray(point.ndim - 1)
+        for (index in 0 until position) {
+            coords[index] = point.dim(index)
+        }
+        for (index in position + 1 until point.ndim) {
+            coords[index - 1] = point.dim(index)
+        }
+        return DefaultPoint(*coords)
+    }
+
+    private fun validatePoint(point: Point) {
+        if (point.ndim != shape.ndim) {
+            throw NDArrayException.IllegalPointDimensionException();
+        }
+        for (index in 0 until point.ndim) {
+            if (point.dim(index) < 0 || point.dim(index) >= shape.dim(index)) {
+                throw NDArrayException.IllegalPointCoordinateException()
+            }
+        }
+    }
+
+    private fun checkShapeEquality(other: NDArray) {
+        for (index in 0 until ndim) {
+            if (dim(index) != other.dim(index)) {
+                val message = "expected `this` and `other` to have same dimensions"
+                throw NDArrayException.IllegalNDArrayDimensionException(message)
+            }
+        }
+    }
+
+    private fun convert1DArrayTo2DArray(firstDim: Int, secondDim: Int, other: NDArray): NDArray {
+        val ndArray = DefaultNDArray(DefaultShape(firstDim, secondDim), IntArray(other.size))
+        for (index in 0 until other.size) {
+            ndArray.values[index] = other.at(DefaultPoint(index))
+        }
+        return ndArray
+    }
+
+    private fun matricesMult(other: NDArray): NDArray {
+        val result = DefaultNDArray(
+            DefaultShape(this.dim(0), other.dim(1)),
+            IntArray(this.dim(0) * other.dim(1))
+        )
+        for (i in 0 until this.dim(0)) {
+            for (j in 0 until other.dim(1)) {
+                var sum = 0
+                for (k in 0 until this.dim(1)) {
+                    sum += this.at(DefaultPoint(i, k)) * other.at(DefaultPoint(k, j))
+                }
+                result.set(DefaultPoint(i, j), sum)
+            }
+        }
+        return result
+    }
 }
 
-sealed class NDArrayException : Exception() {
-    /* TODO: реализовать требуемые исключения */
-    // IllegalPointCoordinateException
-    // IllegalPointDimensionException
+sealed class NDArrayException(reason: String = "") : Exception(reason) {
+    class IllegalPointCoordinateException(reason: String = "") : NDArrayException(reason)
+    class IllegalPointDimensionException(reason: String = "") : NDArrayException(reason)
+    class IllegalNDArrayDimensionException(reason: String = "") : NDArrayException(reason)
 }
--- homework01/src/main/kotlin/Point.kt
+++ homework01/src/main/kotlin/Point.kt
@@ -1,5 +1,4 @@
-
-interface Point: DimentionAware
+interface Point : DimensionAware
 
 /**
  * Реализация Point по умолчаению
@@ -9,5 +8,9 @@ interface Point: DimentionAware
  *
  * Сама коллекция параметров недоступна, доступ - через методы интерфейса
  */
-class DefaultPoint: Point {
+class DefaultPoint(private vararg val coords: Int) : Point {
+    override val ndim: Int
+        get() = coords.size
+
+    override fun dim(i: Int): Int = coords[i]
 }
