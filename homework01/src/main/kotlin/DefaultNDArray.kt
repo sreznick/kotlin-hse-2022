a26d7147ab6027a7a087e920780a9da503553331
Екатерина Шелухина <shelukchina2447@gmail.com>
2022-10-23 17:23:22 +0300

    Addimpl of NDArray

--- homework01/src/main/kotlin/DefaultNDArray.kt
+++ homework01/src/main/kotlin/DefaultNDArray.kt
@@ -1,5 +1,4 @@
-
-interface NDArray: SizeAware, DimentionAware {
+interface NDArray : SizeAware, DimentionAware {
     /*
      * Получаем значение по индексу point
      *
@@ -83,11 +82,97 @@ interface NDArray: SizeAware, DimentionAware {
  *
  * Инициализация - через factory-методы ones(shape: Shape), zeros(shape: Shape) и метод copy
  */
-class DefaultNDArray: NDArray {
+open class DefaultNDArray private constructor(private val shape: Shape, private val points: IntArray) : NDArray {
+    override val ndim: Int = shape.ndim
+    override val size: Int = points.size
+
+    companion object {
+        fun ones(shape: Shape): NDArray = DefaultNDArray(shape, IntArray(shape.size) { 1 })
+        fun zeros(shape: Shape): NDArray = DefaultNDArray(shape, IntArray(shape.size) { 0 })
+    }
+
+    override fun dim(i: Int): Int = shape.dim(i)
+    override fun at(point: Point): Int = points[findIndex(point)]
+    override fun copy(): NDArray = DefaultNDArray(shape, points.clone())
+    override fun view(): NDArray = DefaultNDArray(this.shape, this.points)
+
+    override fun set(point: Point, value: Int) {
+        points[findIndex(point)] = value
+    }
+
+    override fun add(other: NDArray) {
+        if (other.ndim + 1 != ndim && other.ndim != ndim) {
+            throw NDArrayException.IllegalPointDimensionException(other.ndim, shape.ndim)
+        }
+        for (i in 0 until other.ndim) {
+            if (dim(i) != other.dim(i)) {
+                throw NDArrayException.IllegalPointCoordinateException(i, other.dim(i), shape.dim(i))
+            }
+        }
+        var curIndex: Point = DefaultPoint(*IntArray(other.ndim) { 0 })
+        val lastDim = if (ndim != other.ndim) dim(ndim - 1) else 1
+        for (i in 0 until lastDim) {
+            for (j in 0 until other.size) {
+                points[i + j * lastDim] += other.at(curIndex)
+
+                val defaultShape = DefaultShape(*(0 until other.ndim).map { other.dim(it) }.toIntArray())
+                val sizeShape = defaultShape.ndim - 1
+                var indexVar = 1
+                val coords = IntArray(curIndex.ndim) { 0 }
+
+                for (k in sizeShape downTo 0) {
+                    val curDimInd = curIndex.dim(k)
+                    val shapeDimInd = defaultShape.dim(k)
+                    coords[k] = (curDimInd + indexVar) % shapeDimInd
+                    indexVar = (curDimInd + indexVar) / shapeDimInd
+                }
+                curIndex = DefaultPoint(*coords)
+            }
+        }
+    }
+
+    override fun dot(other: NDArray): NDArray {
+        if (ndim != 2 || other.ndim > 2) {
+            throw NDArrayException.IllegalPointDimensionException(other.ndim, shape.ndim)
+        }
+
+        if (other.dim(0) != dim(1)) {
+            throw NDArrayException.IllegalPointCoordinateException(0, other.dim(0), shape.dim(1))
+        }
+        val lastDim = if (other.ndim == 1) 1 else other.dim(1)
+        val result = zeros(DefaultShape(dim(0), lastDim))
+
+        for (i in 0 until dim(0)) {
+            for (j in 0 until lastDim) {
+                var sum = 0
+                for (k in 0 until dim(1)) {
+                    val point = if (other.ndim == 1) DefaultPoint(k) else DefaultPoint(k, j)
+                    sum += at(DefaultPoint(i, k)) * other.at(point)
+                }
+                result.set(DefaultPoint(i, j), sum)
+            }
+        }
+        return result
+    }
+
+    private fun findIndex(point: Point): Int {
+        if (point.ndim != ndim) {
+            throw NDArrayException.IllegalPointDimensionException(point.ndim, ndim)
+        }
+        var index: Int = point.dim(0)
+        for (i in 1 until point.ndim) {
+            index = dim(i) * index + point.dim(i)
+        }
+        if (index >= size) {
+            throw NDArrayException.IllegalPointCoordinateException(index, point.dim(index), point.dim(index))
+        }
+        return index
+    }
 }
 
-sealed class NDArrayException : Exception() {
-    /* TODO: реализовать требуемые исключения */
-    // IllegalPointCoordinateException
-    // IllegalPointDimensionException
+sealed class NDArrayException(reason: String) : Exception(reason) {
+    class IllegalPointCoordinateException(dim: Int, point: Int, shape: Int)
+        : NDArrayException("Exception: Illegal Point Coordinate at $dim, expected $shape, got $point")
+    class IllegalPointDimensionException(pointDim: Int, shapeDim: Int)
+        : NDArrayException("Exception: Illegal Point Dimension: expected $shapeDim, got $pointDim")
 }
