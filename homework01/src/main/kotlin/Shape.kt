75c4760ef0b86915e4663994b2003b5e26546ea1
IlyaAbramovv <45769461+IlyaAbramovv@users.noreply.github.com>
2022-10-23 15:26:30 +0300

    Add files via upload

--- homework01/src/main/kotlin/Shape.kt
+++ homework01/src/main/kotlin/Shape.kt
@@ -1,5 +1,6 @@
-
-interface Shape: DimentionAware, SizeAware
+interface Shape : DimentionAware, SizeAware {
+    fun getDimensionsProductStartsWithIndex(startsWith: Int): Int
+}
 
 /**
  * Реализация Point по умолчаению
@@ -14,10 +15,34 @@ interface Shape: DimentionAware, SizeAware
  *
  * Сама коллекция параметров недоступна, доступ - через методы интерфейса
  */
-class DefaultShape(private vararg val dimentions: Int): Shape {
+class DefaultShape(private vararg val dimensions: Int) : Shape {
+    override val ndim: Int
+        get() = dimensions.size
+
+    override fun dim(i: Int): Int = dimensions[i]
+
+    init {
+        if (dimensions.isEmpty()) throw ShapeArgumentException.EmptyShapeException()
+        dimensions.withIndex().forEach { (index, dim) ->
+            if (dim <= 0) throw ShapeArgumentException.NonPositiveDimensionException(index, dim)
+        }
+    }
+
+    override val size: Int = dimensions.reduce { acc, i -> acc * i }
+
+    private val dimensionsProductCache = IntArray(ndim)
+    override fun getDimensionsProductStartsWithIndex(startsWith: Int): Int {
+        return if (dimensionsProductCache[startsWith] != 0) dimensionsProductCache[startsWith]
+        else {
+            dimensionsProductCache[startsWith] = dimensions.drop(startsWith + 1).fold(1) { acc, i -> acc * i }
+            dimensionsProductCache[startsWith]
+        }
+    }
+
 }
 
-sealed class ShapeArgumentException (reason: String = "") : IllegalArgumentException(reason) {
-    // EmptyShapeException
-    // NonPositiveDimensionException(val index: Int, val value: Int)
+sealed class ShapeArgumentException(reason: String = "") : IllegalArgumentException(reason) {
+    class EmptyShapeException : ShapeArgumentException("Empty shape given")
+    class NonPositiveDimensionException(val index: Int, val value: Int) :
+        ShapeArgumentException("Non positive dimension: $value found in position: $index")
 }
