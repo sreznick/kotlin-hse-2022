a5607a867e01315447595ad250f474eec5520608
Zhenya Dudko <zhenyaaa50@gmail.com>
2022-11-12 23:36:41 +0300

    Homework2 solution

--- homework02/lib/src/main/kotlin/binomial/BinomialHeap.kt
+++ homework02/lib/src/main/kotlin/binomial/BinomialHeap.kt
@@ -23,9 +23,11 @@ package binomial
  * top - взятие минимального элемента
  * drop - удаление минимального элемента
  */
-class BinomialHeap<T: Comparable<T>> private constructor(private val trees: FList<BinomialTree<T>?>): SelfMergeable<BinomialHeap<T>> {
+class BinomialHeap<T : Comparable<T>> private constructor(private val trees: FList<BinomialTree<T>?>) :
+    SelfMergeable<BinomialHeap<T>> {
     companion object {
-        fun <T: Comparable<T>> single(value: T): BinomialHeap<T> = TODO()
+        fun <T : Comparable<T>> single(value: T): BinomialHeap<T> =
+            BinomialHeap(FList.Cons(BinomialTree.single(value), FList.nil()))
     }
 
     /*
@@ -33,14 +35,16 @@ class BinomialHeap<T: Comparable<T>> private constructor(private val trees: FLis
      *
      * Требуемая сложность - O(log(n))
      */
-    override fun plus(other :BinomialHeap<T>): BinomialHeap<T> = TODO()
+    override fun plus(other: BinomialHeap<T>): BinomialHeap<T> {
+        return BinomialHeap(plusWithCarry(this.trees, other.trees))
+    }
 
     /*
      * добавление элемента
      * 
      * Требуемая сложность - O(log(n))
      */
-    operator fun plus(elem: T): BinomialHeap<T> = TODO()
+    operator fun plus(elem: T): BinomialHeap<T> = plus(single(elem))
 
     /*
      * минимальный элемент
@@ -48,7 +52,13 @@ class BinomialHeap<T: Comparable<T>> private constructor(private val trees: FLis
      * Требуемая сложность - O(log(n))
      */
     fun top(): T {
-        TODO()
+        val result: BinomialTree<T> = trees.fold(null) { acc: BinomialTree<T>?, v ->
+            if (acc == null) v
+            else if (v == null) acc
+            else if (acc.value <= v.value) acc
+            else v
+        } ?: throw NoSuchElementException("Heap is empty. There is no minimum")
+        return result.value
     }
 
     /*
@@ -57,7 +67,41 @@ class BinomialHeap<T: Comparable<T>> private constructor(private val trees: FLis
      * Требуемая сложность - O(log(n))
      */
     fun drop(): BinomialHeap<T> {
-        TODO()
+        val minValue = top()
+        val treeWithMinValue: BinomialTree<T> =
+            (trees.filter { it != null && it.value == minValue } as FList.Cons).head
+                ?: throw NoSuchElementException("Heap is empty. There is no minimum")
+
+        return BinomialHeap(trees.map { if (it == treeWithMinValue) null else it })
+            .plus(BinomialHeap(treeWithMinValue.children.reverse().map { it }))
+    }
+
+    private fun plusWithCarry(
+        lhs: FList<BinomialTree<T>?>, rhs: FList<BinomialTree<T>?>, carry: BinomialTree<T>? = null
+    ): FList<BinomialTree<T>?> {
+
+        if (lhs == FList.nil && rhs == FList.nil && carry == null) return FList.nil()
+
+        val lhsTree = if (lhs == FList.nil) null else (lhs as FList.Cons).head
+        val rhsTree = if (rhs == FList.nil) null else (rhs as FList.Cons).head
+
+        val lhsTail = if (lhs == FList.nil) lhs else (lhs as FList.Cons).tail
+        val rhsTail = if (rhs == FList.nil) rhs else (rhs as FList.Cons).tail
+
+        val leftResult =
+            if (lhsTree != null && rhsTree != null && carry != null
+                || lhsTree == null && rhsTree == null && carry != null
+            ) carry
+            else if (lhsTree != null && rhsTree == null && carry == null) lhsTree
+            else if (lhsTree == null && rhsTree != null && carry == null) rhsTree
+            else null
+        val nextCarry =
+            if (lhsTree != null && rhsTree != null) lhsTree.plus(rhsTree)
+            else if (lhsTree != null && carry != null) lhsTree.plus(carry)
+            else if (rhsTree != null && carry != null) rhsTree.plus(carry)
+            else null
+
+        return FList.Cons(leftResult, plusWithCarry(lhsTail, rhsTail, nextCarry))
     }
 }
 
--- homework02/lib/src/main/kotlin/binomial/BinomialTree.kt
+++ homework02/lib/src/main/kotlin/binomial/BinomialTree.kt
@@ -22,9 +22,10 @@ interface SelfMergeable<T> {
  * Дерево совсем без элементов не предусмотрено
  */
 
-class BinomialTree<T: Comparable<T>> private constructor(val value: T, val children: FList<BinomialTree<T>>): SelfMergeable<BinomialTree<T>> {
+class BinomialTree<T: Comparable<T>> private constructor(val value: T, val children: FList<BinomialTree<T>>):
+    SelfMergeable<BinomialTree<T>> {
     // порядок дерева
-    val order: Int = TODO()
+    val order: Int = children.size
 
     /*
      * слияние деревьев
@@ -33,10 +34,16 @@ class BinomialTree<T: Comparable<T>> private constructor(val value: T, val child
      * Требуемая сложность - O(1)
      */
     override fun plus(other: BinomialTree<T>): BinomialTree<T> {
-        TODO()
+        if (this.order != other.order) {
+            throw IllegalArgumentException("Merged trees should be equal order")
+        }
+        if (this.value <= other.value) {
+            return BinomialTree(this.value, FList.Cons(other, this.children))
+        }
+        return BinomialTree(other.value, FList.Cons(this, other.children))
     }
 
     companion object {
-        fun <T: Comparable<T>> single(value: T): BinomialTree<T> = TODO()
+        fun <T: Comparable<T>> single(value: T): BinomialTree<T> = BinomialTree(value, FList.nil())
     }
 }
