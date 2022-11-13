7c938aeeee479e0205ec52293430e2c2f6474938
IlyaAbramovv <45769461+IlyaAbramovv@users.noreply.github.com>
2022-11-13 20:59:27 +0300

    hw2 added

--- homework02/lib/src/main/kotlin/binomial/BinomialTree.kt
+++ homework02/lib/src/main/kotlin/binomial/BinomialTree.kt
@@ -16,7 +16,7 @@ interface SelfMergeable<T> {
  *  - циклы
  *  - стандартные коллекции
  *
- * Детали внутренней реазации должны быть спрятаны
+ * Детали внутренней реализации должны быть спрятаны
  * Создание - только через single() и plus()
  *
  * Дерево совсем без элементов не предусмотрено
@@ -24,7 +24,7 @@ interface SelfMergeable<T> {
 
 class BinomialTree<T: Comparable<T>> private constructor(val value: T, val children: FList<BinomialTree<T>>): SelfMergeable<BinomialTree<T>> {
     // порядок дерева
-    val order: Int = TODO()
+    val order: Int = children.size
 
     /*
      * слияние деревьев
@@ -33,10 +33,18 @@ class BinomialTree<T: Comparable<T>> private constructor(val value: T, val child
      * Требуемая сложность - O(1)
      */
     override fun plus(other: BinomialTree<T>): BinomialTree<T> {
-        TODO()
+        if (other.order != this.order) throw  IllegalArgumentException()
+
+        // Корень нового дерева - минимальный из двух корней данных деревьев
+        return if (this.value < other.value) BinomialTree(value, FList.Cons(other, children))
+        else BinomialTree(other.value, FList.Cons(this, other.children))
     }
 
     companion object {
-        fun <T: Comparable<T>> single(value: T): BinomialTree<T> = TODO()
+        fun <T: Comparable<T>> single(value: T): BinomialTree<T> =  BinomialTree(value, FList.nil())
+    }
+
+    override fun toString(): String {
+        return "($value $children)"
     }
 }
