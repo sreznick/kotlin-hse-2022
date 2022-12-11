7794f966a8bb1c9a5db85c6e9efef5220cc17ab4
Даниил <moiplansh028@gmail.com>
2022-12-11 13:04:04 +0300

    add discussionId

--- homework03/app/src/main/kotlin/homework03/lib/Utils.kt
+++ homework03/app/src/main/kotlin/homework03/lib/Utils.kt
@@ -17,19 +17,19 @@ class Utils {
             GZIPInputStream(content.inputStream()).bufferedReader(Charsets.UTF_8).use { it.readText() }
 
         fun parseTopicMain(content: String): TopicMain = ObjectMapper().readValue(content, TopicMain::class.java)
-        fun parseTopicAbout(content: String): TopicAbout = ObjectMapper().readValue(content, TopicAbout::class.java);
+        fun parseTopicAbout(content: String): TopicAbout = ObjectMapper().readValue(content, TopicAbout::class.java)
 
         //Микробага в json структуре '"replies": "",' с сервера, много копипасты ради ее исправления писать не хочу
         fun parseComments(content: String): List<CommentsJson> =
             ObjectMapper().enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT)
-                .readValue(content, object : TypeReference<List<CommentsJson>>() {});
+                .readValue(content, object : TypeReference<List<CommentsJson>>() {})
 
         fun convertCommentsJsonToCommentsSnapshot(
             commentData: CommentsJson.CommentJsonData.CommentsData.CommentData,
             parentId: Int?,
             idGenerator: IdGenerator = IdGenerator(1),
         ): CommentSnapshot {
-            val currentId = idGenerator.generate();
+            val currentId = idGenerator.generate()
             val children = commentData.replies?.data?.children?.filter { child -> child.data != null }
                 ?.map { child ->
                     convertCommentsJsonToCommentsSnapshot(
@@ -38,26 +38,42 @@ class Utils {
                         idGenerator
                     )
                 }
-            return CommentSnapshot(commentData, currentId, parentId, children);
+            return CommentSnapshot(commentData, currentId, parentId, children)
         }
 
         fun getLinear(commentSnapshotList: List<CommentSnapshot>?, discussionId: String?): List<LinearComment> {
-            val res: MutableList<LinearComment> = mutableListOf();
-            commentSnapshotList?.forEach { res.add(toLinearComment(it, discussionId)); res.addAll(getLinear(it.children, discussionId)) }
-            return res;
+            val res: MutableList<LinearComment> = mutableListOf()
+            commentSnapshotList?.forEach {
+                res.add(toLinearComment(it, discussionId)); res.addAll(
+                getLinear(
+                    it.children,
+                    discussionId
+                )
+            )
+            }
+            return res
         }
 
         private fun toLinearComment(it: CommentSnapshot, discussionId: String?): LinearComment =
-            LinearComment(it.creationTime, it.upVotes, it.downVotes, it.text, it.author, it.id, it.parentId, discussionId)
+            LinearComment(
+                it.creationTime,
+                it.upVotes,
+                it.downVotes,
+                it.text,
+                it.author,
+                it.id,
+                it.parentId,
+                discussionId
+            )
 
-        fun getDiscussionLinks(topicSnapshot: TopicSnapshot, discussionIdGenerator: IdGenerator): List<Pair<String?, String>>? =
-            topicSnapshot.discussions?.filter { d -> d.permalink != null }?.map { d -> Pair(d.id, d.permalink!!) };
+        fun getDiscussionLinks(topicSnapshot: TopicSnapshot): List<Pair<String?, String>>? =
+            topicSnapshot.discussions?.filter { d -> d.permalink != null }?.map { d -> Pair(d.id, d.permalink!!) }
     }
 
     class IdGenerator(private var id: Int = 1) {
         fun generate(): Int {
-            id += 1;
-            return id - 1;
+            id += 1
+            return id - 1
         }
     }
 }
