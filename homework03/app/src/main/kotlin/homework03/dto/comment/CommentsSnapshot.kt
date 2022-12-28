51383c25793c8a9f3d3c0bc22dbc4ce915328fd0
Даниил <moiplansh028@gmail.com>
2022-12-28 15:10:11 +0300

    Revert "refactor"
    This reverts commit 528f7f2f33d8dc8802bcf0413a78780294d01ea3.

--- homework03/app/src/main/kotlin/homework03/dto/comment/CommentsSnapshot.kt
+++ homework03/app/src/main/kotlin/homework03/dto/comment/CommentsSnapshot.kt
@@ -3,17 +3,17 @@ package homework03.dto.comment
 import homework03.lib.Utils
 
 class CommentsSnapshot(commentsRaw: CommentsJson){
-    private val idGenerator = Utils.IdGenerator()
+    val idGenerator = Utils.IdGenerator();
     val comments = commentsRaw.data?.children?.filter { it.data != null }?.map{ Utils.convertCommentsJsonToCommentsSnapshot(it.data!!, null, idGenerator) }
 }
 
-class CommentSnapshot(commentData: CommentsJson.CommentJsonData.CommentsData.CommentData, val id: Int, val parentId: Int?, val children: List<CommentSnapshot>?) {
-    val creationTime = commentData.created?.value
+class CommentSnapshot(val commentData: CommentsJson.CommentJsonData.CommentsData.CommentData, val id: Int, val parentId: Int?, val children: List<CommentSnapshot>?) {
+    val creationTime = commentData.created?.value;
     val upVotes = commentData.ups
     val downVotes = commentData.downs
     val text = commentData.body
-    val textHtml = commentData.body_html
-    val author = commentData.author_fullname
+    val textHtml = commentData.body_html;
+    val author = commentData.author_fullname;
 }
 
 data class LinearComment(val creationTime: Double?, val upVotes: Int?, val downVotes: Int?, val text: String?, val author: String?, val id: Int, val parentId: Int?, val discussionId: String?)
