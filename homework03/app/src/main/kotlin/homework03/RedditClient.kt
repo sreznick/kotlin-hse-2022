c3f78ea2fcf72ff6c719f6fa27d4a440be6c2d5b
Даниил <moiplansh028@gmail.com>
2022-12-28 15:10:10 +0300

    Revert "refactor"
    This reverts commit 2115b952d147265ab63eab41436d6dab1817cbac.

--- homework03/app/src/main/kotlin/homework03/RedditClient.kt
+++ homework03/app/src/main/kotlin/homework03/RedditClient.kt
@@ -1,5 +1,6 @@
 package homework03
 
+import com.ctc.wstx.util.URLUtil
 import com.soywiz.korio.net.http.createHttpClient
 import homework03.dto.comment.CommentsSnapshot
 import homework03.dto.topic.TopicSnapshot
@@ -8,35 +9,35 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 
 class RedditClient {
-    private val httpClient = createHttpClient()
+    private val httpClient = createHttpClient();
 
-    private fun generateLinkByPermLink(suffix: String): String {
+    fun generateLinkByPermlink(suffix: String): String {
         return "https://www.reddit.com${suffix}.json"
     }
 
     suspend fun getTopic(name: String): TopicSnapshot = coroutineScope {
         val topicMainAsync = async {
             val res = httpClient.readBytes("https://www.reddit.com/r/${name}/.json")
-            Utils.parseTopicMain(Utils.ungzip(res))
+            Utils.parseTopicMain(Utils.ungzip(res));
         }
         val topicAboutAsync = async {
             val res = httpClient.readBytes("https://www.reddit.com/r/${name}/about.json")
-            Utils.parseTopicAbout(Utils.ungzip(res))
+            Utils.parseTopicAbout(Utils.ungzip(res));
         }
-        val topicMain = topicMainAsync.await()
-        val topicAbout = topicAboutAsync.await()
-        val timeStamp = System.currentTimeMillis().toDouble()
+        val topicMain = topicMainAsync.await();
+        val topicAbout = topicAboutAsync.await();
+        val timeStamp = System.currentTimeMillis().toDouble();
         TopicSnapshot(topicMain, topicAbout, timeStamp)
     }
 
 
-    suspend fun getComments(permLink: String): CommentsSnapshot? = coroutineScope {
+    suspend fun getComments(permlink: String): CommentsSnapshot? = coroutineScope {
         val commentsMainAsync = async {
-            val res = httpClient.readBytes(generateLinkByPermLink(permLink))
-            Utils.parseComments(Utils.ungzip(res))
+            val res = httpClient.readBytes(generateLinkByPermlink(permlink));
+            Utils.parseComments(Utils.ungzip(res));
         }
-        val commentsMain = commentsMainAsync.await()
-        val comments = commentsMain.getOrNull(1)
+        val commentsMain = commentsMainAsync.await();
+        val comments = commentsMain.getOrNull(1);
         if (comments == null) {
             null
         } else {
