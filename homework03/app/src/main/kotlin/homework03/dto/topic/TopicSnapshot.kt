5e90fc0ff6c453bf040cf0dc21f4a7b2960226b8
Даниил <moiplansh028@gmail.com>
2022-12-28 15:10:15 +0300

    Revert "add discussionId"
    This reverts commit a8f2e959f0adafe265865b05d97ad89d4fc99b6f.

--- homework03/app/src/main/kotlin/homework03/dto/topic/TopicSnapshot.kt
+++ homework03/app/src/main/kotlin/homework03/dto/topic/TopicSnapshot.kt
@@ -1,13 +1,13 @@
 package homework03.dto.topic
 
-class TopicSnapshot (topicMain: TopicMain, topicAbout: TopicAbout, val requestTime: Double) {
+class TopicSnapshot constructor(topicMain: TopicMain, topicAbout: TopicAbout, val requestTime: Double) {
     val creationTime = topicAbout.data?.created?.value;
     val onlineSubscribers = topicAbout.data?.active_user_count;
     val description = topicAbout.data?.description;
 
     val discussions = topicMain.data?.children?.map { TopicDiscussion(it) };
 
-    class TopicDiscussion (discussion: TopicMain.TopicMainData.TopicMainDataChild){
+    class TopicDiscussion constructor(discussion: TopicMain.TopicMainData.TopicMainDataChild){
         val author = discussion.data?.author_fullname
         val createdTime = discussion.data?.created?.value
         val upVotes = discussion.data?.ups
