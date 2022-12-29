a3a67aba6e1bcaef9de0c1e15e29805ea4e34752
pol <buchurella2003@gmail.com>
2022-12-29 21:52:36 +0300

    clean hw4

--- homework01/src/main/kotlin/Point.kt
+++
@@ -1,7 +0,0 @@
-
-interface Point: DimensionAware
-
-class DefaultPoint (private vararg val coords: Int): Point  {
-    override val ndim: Int = coords.size;
-    override fun dim(i: Int): Int = coords[i]
-}
